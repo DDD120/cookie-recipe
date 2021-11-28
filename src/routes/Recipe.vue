cc635b6c87af9fd143d4992be487a6e2263b1ecb
DDD120 <dltnals9794@naver.com>
2021-11-28 16:18:04 +0900

    Vue Router 구성

--- src/routes/Recipe.vue
+++ src/routes/Recipe.vue
@@ -1,3 +1,3 @@
 <template>
-  <router-view />
+  <h1>RECIPE</h1>
 </template>
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./Home";
+import Recipe from "./Recipe";
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/recipe",
+      component: Recipe,
+    },
+  ],
+});
