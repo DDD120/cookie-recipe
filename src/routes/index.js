a579e5a37fa583a223fa9f7cf8110e457971cb41
DDD120 <dltnals9794@naver.com>
2021-12-30 15:54:39 +0900

    스크롤 위치 복구

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ import NotFound from "./NotFound";
 
 export default createRouter({
   history: createWebHashHistory(),
+  scrollBehavior() {
+    return { top: 0 };
+  },
   routes: [
     {
       path: "/",
