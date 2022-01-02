589621bbe04704fda20ab74df92ec707625cdf02
DDD120 <dltnals9794@naver.com>
2022-01-02 22:43:46 +0900

    netlify.toml 수정

--- functions/recipe.js
+++ functions/recipe.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 exports.handler = async function (event) {
   console.log(event);
   const payload = JSON.parse(event.body);
+
   const { title, rcp_nm } = payload;
   const RECIPE_API_KEY = "fee7f81a72a24e28962e";
   const url = rcp_nm
@@ -10,16 +11,10 @@ exports.handler = async function (event) {
     : `http://openapi.foodsafetykorea.go.kr/api/${RECIPE_API_KEY}/COOKRCP01/json/1/300/RCP_NM="${title}"`;
 
   try {
-    const { data } = await axios.get(url);
-    if (data.Error) {
-      return {
-        statusCode: 400,
-        body: data.Error,
-      };
-    }
+    const res = await axios.get(url);
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(res),
     };
   } catch (error) {
     return {
