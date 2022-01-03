d077926577562223b908c6a1b8e98cf6d2e98298
DDD120 <dltnals9794@naver.com>
2022-01-03 13:12:08 +0900

    환경변수, netlify functions

--- functions/recipe.js
+++ functions/recipe.js
@@ -1,22 +1,23 @@
 const axios = require("axios");
+const { VUE_APP_RECIPE_API_KEY } = process.env;
 
 exports.handler = async function (event) {
-  console.log(event);
   const payload = JSON.parse(event.body);
-
   const { title, rcp_nm } = payload;
-  const RECIPE_API_KEY = "fee7f81a72a24e28962e";
   const url = rcp_nm
-    ? `http://openapi.foodsafetykorea.go.kr/api/${RECIPE_API_KEY}/COOKRCP01/json/1/1/RCP_NM="${rcp_nm}"`
-    : `http://openapi.foodsafetykorea.go.kr/api/${RECIPE_API_KEY}/COOKRCP01/json/1/300/RCP_NM="${title}"`;
+    ? `http://openapi.foodsafetykorea.go.kr/api/${VUE_APP_RECIPE_API_KEY}/COOKRCP01/json/1/1/RCP_NM="${rcp_nm}"`
+    : `http://openapi.foodsafetykorea.go.kr/api/${VUE_APP_RECIPE_API_KEY}/COOKRCP01/json/1/300/RCP_NM="${title}"`;
+
+  const URL = encodeURI(url);
 
   try {
-    const res = await axios.get(url);
+    const { data } = await axios.get(URL);
     return {
       statusCode: 200,
-      body: JSON.stringify(res),
+      body: JSON.stringify(data),
     };
   } catch (error) {
+    console.log(error);
     return {
       statusCode: error.response.status,
       body: error.message,
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./routes";
 import "./index.css";
 import store from "./store";
+import dotenv from "dotenv";
 
 createApp(App).use(router).use(store).mount("#app");
+dotenv.config();
